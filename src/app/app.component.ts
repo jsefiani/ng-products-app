5f314da29c55d642e41c0e6dce5829a7eee0c438
Jawad Sefiani <jawad.sefiani@hotmail.com>
2018-10-04 11:21:15 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "pm-root",
+  templateUrl: "./app.component.html"
+})
+
+export class AppComponent {
+  pageTitle: string = "This is a title"
+}
